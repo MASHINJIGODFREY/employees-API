bbce0ccf98c6a0fc21433d7b9a6aa80a6925127c
MASHINJIGODFREY <gmashinji@gmail.com>
2024-07-31 13:29:28 +0300

    Bug-fix for Register User Route

--- app/Http/Controllers/API/AuthController.php
+++ app/Http/Controllers/API/AuthController.php
@@ -22,7 +22,7 @@ class AuthController extends Controller
      */
     public function register(RegisterRequest $request): JsonResponse
     {
-        dd($request->only('email'));
+        // dd($request->only('email'));
         
         $userData = $request->validated();
 
